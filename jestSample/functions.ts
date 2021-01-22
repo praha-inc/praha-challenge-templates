0fa30fc78bf8ce2ba84fd60a8b6571854a971945
ampersand <ampersand.dev@gmail.com>
2021-01-22 14:21:16 +0900

    誤字の修正 ☓：nameApiSerivce ○：nameApiService

--- jestSample/functions.ts
+++ jestSample/functions.ts
@@ -28,8 +28,8 @@ export const asyncSumOfArraySometimesZero = (
 export const getFirstNameThrowIfLong = async (
   maxNameLength: number
 ): Promise<string> => {
-  const nameApiSerivce = new NameApiService(); // fixme: この関数をテストするには、NameApiServiceの使い方を変える必要がありそう！ヒント：依存性の注入
-  const firstName = await nameApiSerivce.getFirstName();
+  const nameApiService = new NameApiService(); // fixme: この関数をテストするには、NameApiServiceの使い方を変える必要がありそう！ヒント：依存性の注入
+  const firstName = await nameApiService.getFirstName();
 
   if (firstName.length > maxNameLength) {
     throw new Error("first_name too long");
