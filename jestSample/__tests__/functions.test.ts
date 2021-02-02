9f620c49c9e7d71fb7004eb3ef4a9c9e810394d4
Awtanabe <panser098@gmail.com>
2021-02-02 22:10:07 +0900

    sumOfArrayが正しく動作する

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,10 @@
 // todo: ここに単体テストを書いてみましょう！
+
+import {sumOfArray} from "../functions"
+
+
+describe("#sumOfArray", () => {
+  test("[1,2]を渡すと3が返ってくる", () => {
+    expect(sumOfArray([1,2])).toBe(3)
+  })
+})
