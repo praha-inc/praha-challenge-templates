7fb58523d74bdfa87eee88c265892ca8ca5f4a59
Awtanabe <panser098@gmail.com>
2021-02-02 22:13:34 +0900

    asyncSumOfArrayのOKテスト

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,10 +1,18 @@
 // todo: ここに単体テストを書いてみましょう！
 
-import {sumOfArray} from "../functions"
+import { sumOfArray,asyncSumOfArray } from "../functions"
 
 
 describe("#sumOfArray", () => {
   test("[1,2]を渡すと3が返ってくる", () => {
     expect(sumOfArray([1,2])).toBe(3)
+  })
+})
+
+describe("#asyncSumOfArray", () => {
+  test("[1,2]を渡すと3が返ってくる", () => {
+    asyncSumOfArray([1.2]).then((result) => {
+      expect(result).toBe(3)
+    })
   })
 })
