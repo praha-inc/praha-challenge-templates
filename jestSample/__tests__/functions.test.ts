d5e169cd76e23a6e4288814fce58c596beabc516
Yasunari_Kainuma <yasu6491@icloud.com>
2022-02-04 09:55:33 +0900

    jestで単体テストを書こう

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,46 @@
 // todo: ここに単体テストを書いてみましょう！
+import {
+  sumOfArray,
+  asyncSumOfArray,
+  asyncSumOfArraySometimesZero,
+  getFirstNameThrowIfLong,
+} from '../functions';
+
+describe('sumOfArray', () => {
+  test('argument [1, 1] to equal 2', () => {
+    expect(sumOfArray([1, 1])).toBe(2);
+  })
+
+  test('argument [-1, -1] to equal -2', () => {
+    expect(sumOfArray([-1, -1])).toBe(-2);
+  })
+
+  test('argument [] to equal 0', () => {
+    expect(sumOfArray([])).toBe(0);
+  })
+})
+
+describe('asyncSumOfArray', () => {
+  test('async argument [1, 1] to equal 2', () => {
+    return expect(asyncSumOfArray([1, 1])).resolves.toBe(2);
+  })
+
+  test('async argument [-1, -1] to equal -2', () => {
+    return expect(asyncSumOfArray([-1, -1])).resolves.toBe(-2);
+  })
+
+  test('async argument [] to equal 0', () => {
+    return expect(asyncSumOfArray([])).resolves.toBe(0);
+  })
+})
+
+describe('asyncSumOfArraySometimesZero', () => {
+  test('async argument [1, 1] to equal 2', async () => {
+    try {
+      const res = await asyncSumOfArraySometimesZero([1, 1]);
+      expect(res).toBe(2);
+    } catch (e) {
+      expect(e).toBe(0);
+    }
+  })
+})
--- jestSample/functions.ts
+++ jestSample/functions.ts
@@ -2,6 +2,7 @@ import { NameApiService } from "./nameApiService";
 import { DatabaseMock } from "./util";
 
 export const sumOfArray = (numbers: number[]): number => {
+  if (numbers.length === 0) return 0;
   return numbers.reduce((a: number, b: number): number => a + b);
 };
 
