f2c82ac0a99009b67c647c70e5f33823f0010083
Norifumi Kimura <norical496@gmail.com>
2023-10-25 21:51:28 +0900

    asyncSumOfArrayのテストを追加

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,4 +1,4 @@
-import { sumOfArray } from "../functions";
+import { sumOfArray, asyncSumOfArray } from "../functions";
 
 // todo: ここに単体テストを書いてみましょう！
 describe('sum of array tests', () => {
@@ -15,5 +15,29 @@ describe('sum of array tests', () => {
     test('empty array throws error', () => {
         const array:number[] = [];
         expect(() => {sumOfArray(array)}).toThrow();
+    });
+});
+
+describe('async sum of array test', () => {
+    test('adds [1] is 1', async () => {
+        const array = [1];
+        const response = await asyncSumOfArray(array);
+        expect(response).toBe(1);
+    });
+
+    test('adds [1, 2, 3] is 6', async () => {
+        const array = [1, 2, 3];
+        const response = await asyncSumOfArray(array);
+        expect(response).toBe(6);
+    });
+
+    test('empty array fails with error', async () => {
+        expect.assertions(1);
+        const array:number[] = [];
+        try{
+            await asyncSumOfArray(array);
+        } catch (e) {
+            expect(e).toBeInstanceOf(TypeError);
+        }
     });
 });
