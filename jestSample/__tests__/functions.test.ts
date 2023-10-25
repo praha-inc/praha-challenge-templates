cf03058e20a10283f6ea7e48be83816c17fec9ba
Norifumi Kimura <norical496@gmail.com>
2023-10-25 21:20:23 +0900

    sumOfArrayのテストを追加

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,19 @@
+import { sumOfArray } from "../functions";
+
 // todo: ここに単体テストを書いてみましょう！
+describe('sum of array tests', () => {
+    test('adds [1] is 1', () => {
+        const array = [1];
+        expect(sumOfArray(array)).toEqual(1);
+    });
+
+    test('adds [1, 2, 3] is 6', () => {
+        const array = [1, 2, 3];
+        expect(sumOfArray(array)).toEqual(6);
+    });
+
+    test('empty array throws error', () => {
+        const array:number[] = [];
+        expect(() => {sumOfArray(array)}).toThrow();
+    });
+});
