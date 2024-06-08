f5ce27cc7ed0cd45e40cb9242a4dd65f9fbefcc0
Sakiyama Shingo <yamaa.exception@gmail.com>
2024-06-09 01:34:22 +0900

    chore: asyncSumOfArrayのテストを追加

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,4 +1,4 @@
-import { sumOfArray } from "../functions";
+import { asyncSumOfArray, sumOfArray } from "../functions";
 
 const VERY_LONG_ARRAY = Array.from({ length: 50 }, (_, i) => i + 1);
 
@@ -73,6 +73,82 @@ describe("sumOfArray", (): void => {
     ])("$title、エラーがスローされる", (argument): void => {
       // @ts-ignore
       expect(() => sumOfArray(argument)).toThrow(TypeError);
+    }
+    );
+  });
+});
+
+describe("asyncSumOfArray", (): void => {
+  describe("自然数の配列を渡した場合", (): void => {
+    it.each([
+      {
+        argument: [1, 2, 3],
+        result: 6,
+      },
+      {
+        argument: VERY_LONG_ARRAY,
+        result: VERY_LONG_ARRAY.reduce((acc, value) => acc + value, 0),
+      },
+    ])("$argumentを渡した場合、$resultが返却される", ({ argument, result }): void => {
+      expect(asyncSumOfArray(argument)).resolves.toBe(result);
+    });
+  });
+
+  describe("自然数と負数の配列を渡した場合", (): void => {
+    it.each([
+      {
+        argument: [1, 2, -30],
+        result: -27,
+      },
+      {
+        argument: [30, -30],
+        result: 0,
+      },
+    ])("$argumentを渡した場合、$resultが返却される", ({ argument, result }): void => {
+      expect(asyncSumOfArray(argument)).resolves.toBe(result);
+    });
+  });
+
+  describe("自然数と負数と0の配列を渡した場合", (): void => {
+    it.each([
+      {
+        argument: [1, -3, 0],
+        result: -2,
+      },
+      {
+        argument: [0, 0, 9, -1],
+        result: 8,
+      },
+    ])("$argumentを渡した場合、$resultが返却される", ({ argument, result }): void => {
+      expect(asyncSumOfArray(argument)).resolves.toBe(result);
+    }
+    );
+  });
+
+  describe("0の配列を渡した場合", (): void => {
+    it("0が返却される", (): void => {
+      expect(asyncSumOfArray([0, 0, 0])).resolves.toBe(0);
+    });
+  });
+
+  describe("空の配列を渡した場合", (): void => {
+    it("エラーがスローされる", (): void => {
+      expect(asyncSumOfArray([])).rejects.toThrow(TypeError);
+    });
+  });
+
+  describe("配列以外を渡した場合", (): void => {
+    it.each([
+      { title: "文字列を渡した場合", argument: "hoge" },
+      { title: "trueを渡した場合", argument: true },
+      { title: "nullを渡した場合", argument: null },
+      { title: "undefinedを渡した場合", argument: undefined },
+      { title: "NaNを渡した場合", argument: NaN },
+      { title: "new Date()を渡した場合", argument: new Date() },
+
+    ])("$title、エラーがスローされる", (argument): void => {
+      // @ts-ignore
+      expect(() => asyncSumOfArray(argument)).rejects.toThrow(TypeError);
     }
     );
   });
