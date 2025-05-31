c28a6116beadb5b0315f966f4ae0da2f1efb4256
ssfuno <96967819+ssfuno@users.noreply.github.com>
2025-05-31 20:59:05 +0900

    add: jestで単体テストを書こう課題2

---
+++ jestSample/__tests__/nameApiService.test.ts
@@ -0,0 +1,32 @@
+import { FetchInterface, NameApiService } from "../nameApiService";
+
+// 課題2-3
+test("取得したfirstNameが4文字以下だったらその値を返す", async () => {
+  class FetchDataMockFourCharacter implements FetchInterface {
+    getData(): any {
+      return {
+        data: {
+          first_name: "Taro"
+        }
+      }
+    }
+  }
+  const fetchDataMock = new FetchDataMockFourCharacter();
+  const nameApiService = new NameApiService(fetchDataMock);
+  expect(await nameApiService.getFirstName()).toBe("Taro");
+});
+
+test("取得したfirstNameが5文字以上だったら例外を発生させる", async () => {
+  class FetchDataMockFiveCharacter implements FetchInterface {
+    getData(): any {
+      return {
+        data: {
+          first_name: "Alice"
+        }
+      }
+    }
+  }
+  const fetchDataMock = new FetchDataMockFiveCharacter();
+  const nameApiService = new NameApiService(fetchDataMock);
+  await expect(nameApiService.getFirstName()).rejects.toThrow();
+});
--- jestSample/nameApiService.ts
+++ jestSample/nameApiService.ts
@@ -1,13 +1,27 @@
 import axios from "axios";
 
-export class NameApiService {
-  private MAX_LENGTH = 4;
-  public constructor() {}
+export interface FetchInterface {
+  getData(): Promise<any>;
+}
 
-  public async getFirstName(): Promise<string> {
-    const { data } = await axios.get(
+class FetchDataWithApi implements FetchInterface {
+  async getData(): Promise<any> {
+    return await axios.get(
       "https://random-data-api.com/api/name/random_name"
     );
+  }
+}
+
+export class NameApiService {
+  private MAX_LENGTH = 4;
+  private fetchService: FetchInterface
+
+  public constructor(fetchService: FetchInterface) {
+    this.fetchService = fetchService;
+  }
+
+  public async getFirstName(): Promise<string> {
+    const { data } = await this.fetchService.getData();
     const firstName = data.first_name as string;
 
     if (firstName.length > this.MAX_LENGTH) {
